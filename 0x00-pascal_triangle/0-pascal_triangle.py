46f925c59ac69761e4718bb1aa0d9ef8fbe678c9
James Koduah <jameskoduah171@gmail.com>
2023-10-18 16:07:08 +0000

    first

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""An implementation of the pascal Triangle"""
+from math import factorial
+
+
+def pascal_triangle(n):
+    """An implementation of the pascal Triangle"""
+    final = []
+    if n <= 0:
+        return final
+
+    for i in range(n):
+        final.append([])
+        for j in range(i+1):
+            final[i].append(factorial(i)//(factorial(j)*factorial(i-j)))
+
+    return final
+
+
+
