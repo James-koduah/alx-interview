0e2ea119b89c04256a6437f2ef7e6bc09f5f6041
James Koduah <jameskoduah171@gmail.com>
2023-10-26 10:14:16 +0000

    0-validate_utf8.py: secn

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -6,12 +6,11 @@ def validUTF8(data):
     """Check if the integers constitue a valid utf-8 encoding"""
     successive_10 = 0
     for b in data:
-        b = bin(b).replace('0b','').rjust(8, '0')
+        b = bin(b).replace('0b', '').rjust(8, '0')
         if successive_10 != 0:
             successive_10 -= 1
             if not b.startswith('10'):
                 return False
         elif b[0] == '1':
-                successive_10 = len(b.split('0')[0]) - 1
+            successive_10 = len(b.split('0')[0]) - 1
     return True
-
