492c86a34ca2605fefebab1836a92f478a1f5edd
james-koduah <jameskoduah171@gmail.com>
2024-01-09 12:46:35 +0000

    0-nqueens: edge cases

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+"""The N queens puzzle is the challenge of placing
+N non-attacking queens on an N×N chessboard."""
+
+import sys
+
+
+if len(sys.argv) != 2:
+    print('Usage: nqueens N')
+    exit(1)
+
+number = sys.argv[1]
+
+if type(number) != int:
+    print('N must be a number')
+    exit(1)
+
+if number < 4:
+    print('N must be at least 4')
+    exit(1)
+
+
