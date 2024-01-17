abaeb48eeaaab0d41f461ce039fa479df65b043d
james-koduah <jameskoduah171@gmail.com>
2024-01-17 16:13:07 +0000

    semistandard fixed

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,40 +1,32 @@
 #!/usr/bin/node
 
-const request = require('request')
+const request = require('request');
 
-if (process.argv.length != 3){
-	return;
-}else{
-	 
-	/**
-	 * function to execute reqeusts and return a promise for synchronous execution
-	 */ 
-	function a_request(url){
-		return new Promise ((resolve, reject)=>{
-			request(url, (error, response, body)=>{
-				if (error) reject(error);
-				resolve(JSON.parse(body))
-			})
-		})
-	}
+if (process.argv.length === 3) {
+  function aRequest (url) {
+    return new Promise((resolve, reject) => {
+      request(url, (error, response, body) => {
+        if (error) reject(error);
+        resolve(JSON.parse(body));
+      });
+    });
+  }
 
-	async function main(){
-		let movie_id = process.argv[2]
-		let movie_url = 'https://swapi-api.alx-tools.com/api/films/' + movie_id
-		let movie;
-		let characters;
-		try{
-			movie = await a_request(movie_url)
-		} catch (error) {
-			console.error(error)
-		}
-		characters = movie['characters']
-		for (character of characters){
-			details = await a_request(character)
-			console.log(details['name'])
-		}
-
-	}
-	main()
-
+  async function main () {
+    const movieId = process.argv[2];
+    const movieUrl = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
+    let characters = '';
+    let movie;
+    try {
+      movie = await aRequest(movieUrl);
+    } catch (error) {
+      console.error(error);
+    }
+    characters = movie.characters;
+    for (const character of characters) {
+      const details = await aRequest(character);
+      console.log(details.name);
+    }
+  }
+  main();
 }
