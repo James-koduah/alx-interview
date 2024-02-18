f8e69188368b63656424914d11aacbd425dde7f6
james-koduah <jameskoduah171@gmail.com>
2024-02-18 05:56:18 +0000

    Working version: first

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,51 @@
+""" Python program to print all
+    primes smaller than or equal to
+    n using Sieve of Eratosthenes
+"""
+def SieveOfEratosthenes(n):
+ 
+    # Create a boolean array
+    # "prime[0..n]" and initialize
+    #  all entries it as true.
+    # A value in prime[i] will
+    # finally be false if i is
+    # Not a prime, else true.
+    prime = [True for i in range(n+1)]
+    p = 2
+    while (p * p <= n):
+ 
+        # If prime[p] is not
+        # changed, then it is a prime
+        if (prime[p] == True):
+ 
+            # Update all multiples of p
+            for i in range(p * p, n+1, p):
+                prime[i] = False
+        p += 1
+ 
+    final = []
+    # Print all prime numbers
+    for p in range(2, n+1):
+        if prime[p]:
+            final.append(p)
+    return final
+ 
+    
+def isWinner(x, nums):
+    maria = 0
+    ben = 0
+    for i in range(x):
+        n = nums[i]
+        integer_set = [i for i in range(1, n+1)]
+        prime_num = SieveOfEratosthenes(n)
+        if len(prime_num) == 0:
+            ben += 1
+            continue
+        if len(prime_num) % 2 == 0:
+            ben += 1
+            continue
+        maria += 1
+    if maria > ben:
+        return 'Maria'
+    else:
+        return 'Ben'
